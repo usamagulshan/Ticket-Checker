41e31ae394449ca852d9785dc430d43ac4710c40
Usama Bin Gulshan <163780974+usamagulshan@users.noreply.github.com>
2024-03-25 12:52:48 -0800

    First Commit

---
+++ ticket.js
@@ -0,0 +1,28 @@
+/*let ticket_holder_age : number = 0
+if(ticket_holder_age <= 5){
+    console.log("This is a child, Child ticket fare is 0")
+} else if(ticket_holder_age <= 20){
+    console.log("You are a Teenagers, Ticket price for teenagers are 500")
+} else if(ticket_holder_age <= 30){
+    console.log("You are a adult, Ticket price for adults are 1000")
+} else if(ticket_holder_age => 60){
+    console.log("You are a senior citizen, Ticket price for senior citizens are 0")
+} else {
+    console.log("You are late, You lost your Flight")
+}*/
+var ticket_holder_age = 0;
+if (ticket_holder_age == 5) {
+    console.log("This is a child, Child ticket fare is 0");
+}
+else if (ticket_holder_age == 20) {
+    console.log("You are a Teenagers, Ticket price for teenagers are 500");
+}
+else if (ticket_holder_age == 30) {
+    console.log("You are a adult, Ticket price for adults are 1000");
+}
+else if (ticket_holder_age == 60) {
+    console.log("You are a senior citizen, Ticket price for senior citizens are 0");
+}
+else {
+    console.log("You are late, You lost your Flight");
+}
---
+++ ticket.ts
@@ -0,0 +1,29 @@
+// If-else Statement practice #1
+
+/*let ticket_holder_age : number = 0
+if(ticket_holder_age <= 5){
+    console.log("This is a child, Child ticket fare is 0")
+} else if(ticket_holder_age <= 20){
+    console.log("You are a Teenagers, Ticket price for teenagers are 500")
+} else if(ticket_holder_age <= 30){
+    console.log("You are a adult, Ticket price for adults are 1000")
+} else if(ticket_holder_age > 60){
+    console.log("You are a senior citizen, Ticket price for senior citizens are 0")
+} else {
+    console.log("You are late, You lost your Flight")
+}*/
+
+// if-else statement practice #2
+
+let ticket_holder_age : number = 0
+if(ticket_holder_age == 5){
+    console.log("This is a child, Child ticket fare is 0")
+} else if(ticket_holder_age == 20){
+    console.log("You are a Teenagers, Ticket price for teenagers are 500")
+} else if(ticket_holder_age == 30){
+    console.log("You are a adult, Ticket price for adults are 1000")
+} else if(ticket_holder_age == 60){
+    console.log("You are a senior citizen, Ticket price for senior citizens are 0")
+} else {
+    console.log("You are late, You lost your Flight")
+}
